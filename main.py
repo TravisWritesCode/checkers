5e9a659439f17fe85cdf1d90817a4dd0f7cbcbbb
Travis Hescox <thescox@incomm.com>
2021-07-08 09:47:25 -0400

    pieces added

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ WINDOW_WIDTH = 800 #Pixel width of grid
 BLOCK_SIZE = 100  #Set the size of the grid block
 GRID_WIDTH = int(WINDOW_WIDTH / BLOCK_SIZE) #Cell width of grid
 GRID_HEIGHT = int(WINDOW_HEIGHT / BLOCK_SIZE) #Cell height of grid
-
+BOARD_POS = (0, 0)
+SINGLE_RED = pygame.image.load(r"./graphics/red-piece.png")
+KING_RED = pygame.image.load(r"./graphics/red-king.png")
+SINGLE_BLACK = pygame.image.load(r"./graphics/black-piece.png")
+KING_BLACK = pygame.image.load(r"./graphics/black-king.png")
 
 def drawGrid():
     white = True
@@ -30,12 +34,64 @@ def drawGrid():
             white = True
 
 
+#2d array for storing board state
+def setBoard():
+    board = []
+    for y in range(8):
+        board.append([])
+        for x in range(8):
+            board[y].append(None)
+    for black in range(0, 7, 2):
+        board[0][black] = ("black", "single")
+    for black in range(1, 8, 2):
+        board[1][black] = ("black", "single")
+    for black in range(0, 7, 2):
+        board[2][black] = ("black", "single")
+    for red in range(1, 8, 2):
+        board[7][red] = ("red", "single")
+    for red in range(0, 7, 2):
+        board[6][red] = ("red", "single")
+    for red in range(1, 8, 2):
+        board[5][red] = ("red", "single")
+    return board
+
+
+#returns the space where the pointer is located
+def getSpace(board):
+    #gets x, y coordinates of mouse position and
+    pointer = pygame.Vector2(pygame.mouse.get_pos()) - BOARD_POS
+    #use integer division to get board square
+    x, y = [int(coord // BLOCK_SIZE) for coord in pointer]
+    try:
+        if x >= 0 and y >= 0:
+            # returns the boord square and x, y coordinates of pointer location
+            return (board[y][x], x, y)
+    except IndexError:
+        pass
+    return None, None, None
+
+
+#places pieces on the board
+def placePieces(SCREEN, board):
+    for y in range(8):
+        for x in range(8):
+            currPiece = board[y][x]
+            if currPiece:
+                color, type = currPiece
+                s1 = SINGLE_RED
+                s2 = SINGLE_RED
+                pos = pygame.Rect(BOARD_POS[0] + x * BLOCK_SIZE + 1, BOARD_POS[1] + y * BLOCK_SIZE + 1, BLOCK_SIZE, BLOCK_SIZE)
+                SCREEN.blit(s2, s2.get_rect(center=pos.center).move(1, 1))
+                SCREEN.blit(s1, s1.get_rect(center=pos.center))
+
+
 def main():
     global SCREEN
     pygame.init()
     pygame.display.set_caption("Travis Hescox- Checkers")
     SCREEN = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
     SCREEN.fill(WHITE)
+    board = setBoard()
 
     while True:
         drawGrid()
@@ -43,7 +99,13 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        piece, x, y = getSpace(board)
 
+        if x != None:
+            rect = (BOARD_POS[0] + x * BLOCK_SIZE, BOARD_POS[1] + y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(SCREEN, (255, 0, 0, 50), rect, 2)
+        placePieces(SCREEN, board)
+        pygame.display.flip()
         pygame.display.update()
 
 
